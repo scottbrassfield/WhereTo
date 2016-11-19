c4df89f5340973f2066dbbe31f7d2310e2716ba1
Scott Brassfield <scott.brassfield@gmail.com>
2016-11-19 09:07:01 -0800

    Formatted Places modal to show name and address. #3

--- src/js/components/Places.js
+++ src/js/components/Places.js
@@ -2,28 +2,35 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Modal } from 'react-bootstrap'
 import { initiateTrip, addMarkers, toggleResults} from '../actions/actionCreators'
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui'
 
 const Places = ({ show, places, onClick, onHide, title }) => {
   return (
-    <Modal show={show} onHide={onHide}>
+    <Modal bsSize='large' show={show} onHide={onHide}>
       <Modal.Header>
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        { places && places.map(place => (
-            <div
-              key={place.id}
-              onClick={onClick}
-            >
-              {place.name}
-            </div>
-          )
-        )}
+        <Table onRowSelection={onClick}>
+          <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+            <TableRow>
+              <TableHeaderColumn>Location</TableHeaderColumn>
+              <TableHeaderColumn>Address</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody showRowHover={true} displayRowCheckbox={false}>
+            { places && places.map(place => (
+              <TableRow key={place.id}>
+                <TableRowColumn>{place.name}</TableRowColumn>
+                <TableRowColumn>{place.formatted_address}</TableRowColumn>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Modal.Body>
     </Modal>
   )
 }
-
 
 const mapDispatch = (dispatch, ownProps) => {
   return {
