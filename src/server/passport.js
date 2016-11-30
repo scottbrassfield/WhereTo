3c052677e5e3f8792166056984729680eb555cf9
Scott Brassfield <scott.brassfield@gmail.com>
2016-11-29 23:24:12 -0800

    Updates to passport configuration.

--- src/server/passport.js
+++ src/server/passport.js
@@ -1,13 +1,62 @@
+/* eslint-disable no-console */
 
-module.exports = function(passport, LocalStrategy, User) {
+const User = require('../models/Users')
+const LocalStrategy = require('passport-local').Strategy
+const bCrypt = require('bcrypt-nodejs')
 
-  passport.use(new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }, (user, err) => {
-      if (err) { return done(err) }
-      if (!user) { return done(null, false) }
-      if (!user.verifyPassword(password)) { return done(null, false) }
-      return done(null, user)
-    })
+module.exports = function(passport) {
+
+  passport.use('local-login', new LocalStrategy({ passReqToCallback: true },
+    function(req, username, password, done) {
+      User.findOne({ username: username },
+        function(err, user) {
+          if (err) {
+            return done(err)
+          }
+          if (!user) {
+            console.log('User Not Found with username '+username);
+            return done(null, false,
+              req.flash('message', 'User Not Found'))
+          }
+          if (!checkPassword(user, password)) {
+            return done(null, false,
+              req.flash('message', 'Invalid Password'))
+          }
+          return done(null, user)
+        }
+      )
+    }
+  ))
+
+
+  passport.use('local-register', new LocalStrategy({ passReqToCallback: true },
+    function(req, username, password, done) {
+      process.nextTick(function() {
+        User.findOne({ username: username }, function(err, user) {
+          if (err) {
+            console.log('Error in signup' + err);
+            return done(err)
+          }
+          if (user) {
+            console.log('User already exists');
+            return done(null, false,
+              req.flash('signupMessage', 'User already exists.'))
+          } else {
+            var newUser = new User({
+              username: username,
+              password: createHash(password),
+              email: req.param('email'),
+            })
+            newUser.save(function(err) {
+              if (err) {
+                console.log('Error in saving user' + err);
+                return done(err)
+              }
+              return done(null, newUser)
+            })
+          }
+        })
+      })
   }))
 
   passport.serializeUser((user, done) => {
@@ -20,22 +69,12 @@ module.exports = function(passport, LocalStrategy, User) {
     })
   })
 
+  function checkPassword(user, password) {
+    return bCrypt.compareSync(password, user.password)
+  }
+
+  function createHash(password) {
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null)
+  }
+
 }
-
-  passport.use('registerUser', new LocalStrategy(
-    { passReqToCallback: true },
-    (req, username, password, done) => {
-      var newUser = new User({
-        username: username,
-        password: password,
-        email: req.body.email
-      })
-
-      newUser.save(err => {
-        if (err) {
-          return done(err)
-        }
-        return done(null, newUser)
-      })
-    }
-  ))
