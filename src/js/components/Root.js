a62fda1d66d3c913bc9ecc36d745795f83a38e37
Scott Brassfield <scott.brassfield@gmail.com>
2016-12-02 09:38:39 -0800

    Add client routes with react router.

---
+++ src/js/components/Root.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+import App from './app'
+import Home from './Home'
+import Dashboard from './Dashboard'
+import NewTrip from './NewTrip'
+import TripList from './TripList'
+import Trip from './Trip'
+
+const Root = ({store}) => (
+  <Provider store={ store }>
+    <MuiThemeProvider>
+      <Router history={browserHistory}>
+        <Route path='/' component={App} >
+          <IndexRoute component={Home} />
+          <Route path='/user' component={Dashboard}>
+            <IndexRoute component={TripList} />
+            <Route path='/trips/:tripId' component={Trip} />
+            <Route path='/newTrip' component={NewTrip} />
+          </Route>
+        </Route>
+      </Router>
+    </MuiThemeProvider>
+  </Provider>
+)
+
+export default Root
